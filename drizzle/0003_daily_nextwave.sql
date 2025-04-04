840fe95296be5cc1dd1e9e9a25ef5aeaf4fc1506
Rommert Zijlstra <rommert.thomas@gmail.com>
2025-04-04 20:28:17 +0200

    fix: properly add `userId` to member

---
+++ drizzle/0003_daily_nextwave.sql
@@ -0,0 +1,27 @@
+-- Custom SQL migration file, put your code below! --
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.member ("email", "name", "userId")
+  values (
+    new.email,
+    coalesce(
+      new.raw_user_meta_data ->> 'name',
+      new.raw_user_meta_data ->> 'full_name',
+      new.raw_user_meta_data ->> 'user_name',
+      '[No Name]'
+    ),
+    new.id
+  );
+  return new;
+end;
+$$;
+
+drop trigger if exists on_auth_user_created on auth.users;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
