7409c4c6aa34676d33beafadde78ad6d8eb12474
Rommert Zijlstra <rommert.thomas@gmail.com>
2025-04-09 00:14:02 +0200

    fix: teams layout

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -33,28 +33,32 @@
 	const { form: formData, enhance } = form;
 </script>
 
-Teams
+<div class="flex gap-8 p-8">
+	<div class="prose">
+		<h2>Teams</h2>
+	</div>
 
-<Dialog.Root>
-	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>New Team</Dialog.Trigger>
-	<Dialog.Content class="sm:max-w-[425px]">
-		<Dialog.Header>
-			<Dialog.Title>New Team</Dialog.Title>
-		</Dialog.Header>
-		<form action="?/create" method="POST" use:enhance>
-			<Form.Field {form} name="name">
-				<Form.Control>
-					{#snippet children({ props })}
-						<Form.Label>Team Name</Form.Label>
-						<Input {...props} bind:value={$formData.name} />
-					{/snippet}
-				</Form.Control>
-				<Form.FieldErrors />
-			</Form.Field>
+	<Dialog.Root>
+		<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>New Team</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>New Team</Dialog.Title>
+			</Dialog.Header>
+			<form action="?/create" method="POST" use:enhance>
+				<Form.Field {form} name="name">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Team Name</Form.Label>
+							<Input {...props} bind:value={$formData.name} />
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
 
-			<Form.Button type="submit">Create</Form.Button>
-		</form>
-	</Dialog.Content>
-</Dialog.Root>
+				<Form.Button type="submit">Create</Form.Button>
+			</form>
+		</Dialog.Content>
+	</Dialog.Root>
+</div>
 
 <DataTable {columns} data={data.teams} />
